e96019e1fec59bbd1b547ba46e79f35b25aacf9f
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 18:50:15 +0900

    8

--- logic08.py
+++ logic08.py
@@ -7,4 +7,7 @@ def main(a,b):
     Returns:
         True if at least one of the numbers 'a' and 'b' is even, False otherwise
     """
-    return
+    x=a%2==0 or b%2==0 
+    return x
+x=main(3,10)
+print(x)
