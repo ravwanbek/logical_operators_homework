977d067b433193b60b05c6622493fbf82c36b21a
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 20:32:39 +0900

    12

--- logic12.py
+++ logic12.py
@@ -6,7 +6,7 @@ def main(a):
     Returns:
         True if all digits of a are the same, False otherwise
     """
-    x= 1<=(100-a)<=90 and (a==11 or a==22 or a==33 or a==44 or a==55 or a==66 or a==77 or a==88) 
+    x= 1<=(100-a)<=90 and (a==11 or a==22 or a==33 or a==44 or a==55 or a==66 or a==77 or a==88 or a==99) 
     return x
-x=main(65)
+x=main(99)
 print(x)
