efe9ec2c7b6ca885fd33e3628337c610f7b843a4
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 18:40:50 +0900

    3

--- logic03.py
+++ logic03.py
@@ -7,4 +7,11 @@ def main(a,b):
     Returns:
         True if each of the numbers 'a' and 'b' is negative, False otherwise
     """
-    return
+
+
+
+    x= a<0 and b<0
+    return x
+x=main(-1,-5)
+print(x)
+
