c47b010ef3630c7ab7b7aa78a5193211d8458aae
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 19:25:18 +0900

    13

--- logic13.py
+++ logic13.py
@@ -6,4 +6,23 @@ def main(a):
     Returns:
         True if all digits sum is even, False otherwise
     """
-    return
+
+    c1=a%10
+    c2=a//10
+    c3=c1+c2
+    
+    
+    x= 1<=(100-a)<=90 and c3%2==0
+
+    
+    
+
+
+
+
+
+    return x
+
+x=main(100)
+print(x)
+    
