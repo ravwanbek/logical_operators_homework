c63b955d63e2ce23c7f90e01fc0434951107e8d0
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 18:51:50 +0900

    9

--- logic09.py
+++ logic09.py
@@ -7,4 +7,7 @@ def main(a,b):
     Returns:
         True if at least one of the numbers 'a' and 'b' is odd, False otherwise
     """
-    return
+    x=a%2!=0 or b%2!=0 
+    return x
+x=main(3,2)
+print(x)
