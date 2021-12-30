5993532181a5004114ca57ea15c01c6a090e3814
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 20:29:53 +0900

    17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,12 @@ def main(a):
     Returns:
         True if all digits of a are in ascending order, False otherwise
     """
-    return
+    c1=a%10
+    c2=int((a%100-c1)/10)
+    c3=a//100-(a//1000)*10
+    c4=(a//1000)-(a//10000)*10
+    c5=a//10000
+    x=1<=(100000-a)<=90000 and (c1>c2>c3>c4>c5) 
+    return x
+x=main(10349)
+print(x)
