0c6edf25513c5552a13c8206a2de27046ab34f23
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 20:04:39 +0900

    15

--- logic15.py
+++ logic15.py
@@ -6,4 +6,15 @@ def main(a):
     Returns:
         True if all digits sum is odd, False otherwise
     """
-    return
+
+    c1=a%10
+    c2=(a%100-c1)/10
+    c3=a//100
+    c_sum=c1+c2+c3
+
+    x= 1<=(1000-a)<=900 and c_sum%2!=0
+
+    return x
+x=main(96)
+print(x)
+
