3fdddbe257bab68760527e1ad25abadff1a626b1
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-30 19:28:18 +0900

    14

--- logic14.py
+++ logic14.py
@@ -6,4 +6,14 @@ def main(a):
     Returns:
         True if all digits sum is odd, False otherwise
     """
-    return
+    c1=a%10
+    c2=a//10
+    c3=c1+c2
+    
+    x= 1<=(100-a)<=90 and c3%2!=0
+    
+    
+    return x
+
+x=main(36)
+print(x)
